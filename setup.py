3ad68f1100d3c32b9ca27d074ac3645841b1db62
John Forbes <john.robert.forbes@gmail.com>
2023-08-10 21:37:56 +1000

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = Path("./README.md").read_text()
 
 setup(
   name='xgu',
-  version='0.0.5',
+  version='0.0.6',
   license='MIT',
   description='Semi-Automatic Git Tool',
   long_description=long_description,
