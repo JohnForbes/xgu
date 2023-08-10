9dffb301c5eeff77ab119082b862f006d6ade751
John Forbes <john.robert.forbes@gmail.com>
2023-08-10 21:33:00 +1000

    Updated xgu/add_and_commit_changes.py.

--- xgu/add_and_commit_changes.py
+++ xgu/add_and_commit_changes.py
@@ -5,7 +5,7 @@ from xgu.f_d import f as f_D
 term_to_function = {' M': f_M, '??': f_A, ' D': f_D}
 
 # process_changes
-def f(d):
+def f(d, ask):
   for k in [' M', '??', ' D']:
     if k in d:
-      term_to_function[k](sorted(d[k])[0])
+      term_to_function[k](sorted(d[k])[0], ask)
