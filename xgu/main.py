d4a34b348c7b6e96de10342519513c1a6da8f19f
John Forbes <john.robert.forbes@gmail.com>
2023-08-10 21:36:29 +1000

    Updated xgu/main.py.

--- xgu/main.py
+++ xgu/main.py
@@ -5,11 +5,10 @@ from xgu.git_status import f as git_status
 from xgu.add_and_commit_changes import f as add_and_commit_changes
 
 # main
-def f():
+def f(ask=True):
   git_pull()
   status_result = git_status()
   q = prepare_q(status_result)
   changes = populate_changes_dict(q)
-  add_and_commit_changes(changes)
-
+  add_and_commit_changes(changes, ask)
   if any([_ in changes for _ in [' M', '??', ' D']]): f()
